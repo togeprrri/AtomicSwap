bfa1a766a1fed5cff7d97ce408ff979208af119e
_ <ivasyukvolodimir@gmail.com>
2022-09-25 01:16:03 +0300

    Writted contract and config

---
+++ contracts/HTLC.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.17;
+
+contract HTLC {
+    string public version;
+    bytes32 public digest;
+    address public destination;
+    uint public timeOut;
+    address private issuer; 
+
+    modifier onlyIssuer {
+        require(msg.sender == issuer, "Sender is not issuer"); 
+        _; 
+    }
+
+    constructor (bytes32 _hash, address _dest, uint256 _timeLimit) {
+        require(digest != 0 || _dest != address(0) || _timeLimit != 0, "Wrong deployment arguments");
+        digest = _hash;
+        destination = _dest;
+        timeOut = block.timestamp + (_timeLimit * 1 hours);
+        issuer = msg.sender; 
+    }
+ 
+    function claim(string memory _hash) public returns(bool result) {
+       require(digest == keccak256(abi.encodePacked(_hash)), "Wrong hash");
+       selfdestruct(payable(destination));
+       return true;
+       }
+
+    function refund() onlyIssuer public returns(bool result) {
+        require(block.timestamp >= timeOut, "It's not time yet");
+        selfdestruct(payable(issuer));
+        return true;
+    }
+
+    receive () external payable {
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,21 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+
+require("keys.js");
+
+
+
+
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.13",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    },
+    ropset: {
+      url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [ROPSTEN_PRIVATE_KEY]
+    }
+  }
 };
